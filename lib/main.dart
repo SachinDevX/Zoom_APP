415dfdebde4806c93548f148f805d1a292ec3360
Sachin Sharma <mrsachinsharma3214@gmail.com>
2024-11-12 13:27:17 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_app/Utility/color.dart';
+import 'package:zoom_app/screens/Home_screen.dart';
+import 'package:zoom_app/screens/Login_Screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zoom Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor : backgroundColor,
+      ),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
+      home: const LoginScreen(),
+    );
+  }
+}
+
